d78c5698cd56ee1f17f70db2557e5ccca842827c
kanimozhi33 <kanimozhi02vm@gmail.com>
2025-04-01 22:51:05 +0530

    reverse of sll, iterative and recursion

---
+++ linked_lists.py/reverse_sll_recursion.py
@@ -0,0 +1,7 @@
+def reverseListRecursive(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    if not head or not head.next:
+        return head
+    reversed_head = self.reverseListRecursive(head.next)  # Reverse the rest of the list
+    head.next.next = head  # Make the next node point to the current node
+    head.next = None  # Set the current node's next pointer to None
+    return reversed_head
