b0998dbc77308066687e590ee10cee07ea810f75
kanimozhi33 <kanimozhi02vm@gmail.com>
2025-06-18 22:38:05 +0530

    1

---
+++ binary tree/bt_rightside_view.js
@@ -0,0 +1,24 @@
+function rightSideView(root) {
+    const result = [];
+    if (!root) return result;
+
+    const queue = [root];
+
+    while (queue.length > 0) {
+        const levelSize = queue.length;
+
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift();
+
+            // If it's the rightmost node at this level
+            if (i === levelSize - 1) {
+                result.push(node.val);
+            }
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+    }
+
+    return result;
+}
