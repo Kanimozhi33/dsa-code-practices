3b02c7a277a234294be0a5175900f05c7b989665
kanimozhi33 <kanimozhi02vm@gmail.com>
2025-07-14 22:24:43 +0530

    1

--- interview_qns.py/twoSum.js
+++ interview_qns.py/twoSum.js
@@ -9,3 +9,22 @@ function twoSum(nums, target) {
   }
   return [];
 }
+
+
+function twoSum(nums, target){
+    let left = 0;
+    let right = nums.length-1;
+    while(left< right){
+        const sum = nums[left] + nums[right];
+        if (sum === target){
+          return [left, right];
+        }
+        else if(left < target){
+          left ++;
+        }
+        else{
+          right --;
+        }
+     }
+     return [];
+}
