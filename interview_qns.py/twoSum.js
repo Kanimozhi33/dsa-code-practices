1be71ccc124995a56b653f4e49d5559fbf27dda8
kanimozhi33 <kanimozhi02vm@gmail.com>
2025-07-14 22:25:55 +0530

    1

--- interview_qns.py/twoSum.js
+++ interview_qns.py/twoSum.js
@@ -28,3 +28,36 @@ function twoSum(nums, target){
      }
      return [];
 }
+
+
+// if 1- indexed,
+function twoSum(nums, target) {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.hasOwnProperty(complement)) {
+      return [map[complement], i+1];
+    }
+    map[nums[i]] = i+1;
+  }
+  return [];
+}
+
+
+function twoSum(nums, target){
+    let left = 0;
+    let right = nums.length-1;
+    while(left< right){
+        const sum = nums[left] + nums[right];
+        if (sum === target){
+          return [left+1, right+1];
+        }
+        else if(left < target){
+          left ++;
+        }
+        else{
+          right --;
+        }
+     }
+     return [];
+}
